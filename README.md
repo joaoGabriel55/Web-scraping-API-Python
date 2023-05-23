5e1cceda5b4d1ede51b57816eef36589bfa279fc
joaoGabriel55 <j.quaresmasantos_98@hotmail.com>
2023-05-23 08:46:41 -0300

    refactor: add recursion scraping

---
+++ README.md
@@ -0,0 +1,10 @@
+# Google Shopping Web Scraping API
+
+## How to run?
+```sh
+flask --app app run # http://127.0.0.1:5000
+```
+
+## API endpoint
+
+GET - /search?product=<name>&min_price=<price>&max_price=<price>
--- app.py
+++ app.py
@@ -8,21 +8,45 @@ app = Flask(__name__)
 regex = r'^([A-Z]{1,3})\$?(\d{1,3}(?:,\d{3})*(?:\.\d{2})?)$'
 
 base_url = 'https://www.google.com'
+headers = {
+    'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; WOW64; Trident/7.0; rv:11.0) like Gecko'
+}
 
 def mount_url(product, min_price, max_price):
-    return f"{base_url}/search?igu=1&hl=en&tbm=shop&psb=1&q={product}&tbs=mr:1,price:1,ppr_min:{min_price},ppr_max:{max_price}&sa=X&ved=0ahUKEwjryIvr5oT_AhWgqpUCHSAZCjYQvSsIrAkoAQ&biw=1339&bih=965&dpr=1"
+    return f"{base_url}/search?igu=1&hl=en&tbm=shop&psb=1&q={product}&tbs=mr:1,price:1,ppr_min:{min_price},ppr_max:{max_price}"
 
 
 def get_currency_and_amount(text):
     matches = re.match(regex, text)
 
     if matches:
-        number = float(matches.group(2).replace(',', ''))
-
-        return number
+        return float(matches.group(2).replace(',', ''))
 
     return 0
 
+
+def recursive_scrape(url):
+    result = []
+
+    response = requests.get(url, headers=headers)
+    soup = BeautifulSoup(response.text, 'lxml')
+    elements = soup.find_all(class_='HRLxBb')
+
+    if len(elements) == 0:
+        print(soup.prettify())
+        return result
+
+    next_link = soup.find(class_='lYtaR').get_attribute_list('href')[0]
+
+    for element in soup.find_all(class_='HRLxBb'):
+        value = get_currency_and_amount(element.get_text())
+        result.append(value)
+
+    # Recursive call to continue scraping next pages
+    if next_link:
+        result += recursive_scrape(f'{base_url}{next_link}')
+
+    return result
 
 
 @app.route("/search")
@@ -33,16 +57,9 @@ def search():
 
     url = mount_url(product, min_price, max_price)
 
-    headers = {
-        'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; WOW64; Trident/7.0; rv:11.0) like Gecko'
-    }
+    result = recursive_scrape(url)
 
-    result = []
-    response = requests.get(url, headers=headers)
-    soup = BeautifulSoup(response.text, 'lxml')
-    
-    for element in soup.find_all(class_='HRLxBb'):
-        value = get_currency_and_amount(element.get_text())
-        result.append(value)
+    if len(result) == 0:
+        return { 'message': 'No results found', 'url': url }
 
-    return {'url': url, 'result': result}
+    return {'url': url, 'result': result, 'next': f'{base_url}{next}'}
